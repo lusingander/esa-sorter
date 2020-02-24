580f3149038e4cf47328966865a624db23e91fad
fujimoto kyosuke <kyoro.f@gmail.com>
2020-02-24 10:53:51 +0900

    Fix asc / desc to be changeable

--- src/script.js
+++ src/script.js
@@ -1,22 +1,44 @@
-const ul = document.querySelector("nav.navbar-side > ul");
-const lis = ul.querySelectorAll("li");
-
-const liSorter = (a, b) => {
-  if (a.key < b.key) {
+const liSorterTitleAsc = (a, b) => {
+  if (a.title < b.title) {
     return -1;
-  } else if (a.key > b.key) {
+  } else if (a.title > b.title) {
     return 1;
   }
   return 0;
 };
 
-[].slice
-  .call(lis)
-  .map(li => ({
-    li: li,
-    key: li.querySelector("span.title > span.navbar-side__title-name").innerHTML
-  }))
-  .sort(liSorter)
-  .forEach(li => {
-    ul.appendChild(li.li);
-  });
+const liSorterTitleDesc = (a, b) => -1 * liSorterTitleAsc(a, b);
+
+const nav = document.querySelector("nav.navbar-side");
+const ul = nav.querySelector("ul");
+const lis = ul.querySelectorAll("li");
+
+const sort = sorter => {
+  [].slice
+    .call(lis)
+    .map(li => ({
+      li: li,
+      title: li.querySelector("span.title > span.navbar-side__title-name")
+        .innerHTML
+    }))
+    .sort(sorter)
+    .forEach(li => {
+      ul.appendChild(li.li);
+    });
+};
+
+const keySelectorElem = document.createElement("div");
+keySelectorElem.appendChild(document.createTextNode("Title Asc"));
+keySelectorElem.addEventListener("click", e => {
+  const current = keySelectorElem.textContent;
+  if (current === "Title Asc") {
+    sort(liSorterTitleDesc);
+    keySelectorElem.textContent = "Title Desc";
+  } else {
+    sort(liSorterTitleAsc);
+    keySelectorElem.textContent = "Title Asc";
+  }
+});
+nav.insertBefore(keySelectorElem, ul);
+
+sort(liSorterTitleAsc);
